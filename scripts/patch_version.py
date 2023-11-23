e4d35484e4632000ebf2adb83c54d230eb62d71f
NimrodAvni78 <73278770+NimrodAvni78@users.noreply.github.com>
2023-11-23 13:48:05 +0200

    install poetry in test publish (#3)

--- scripts/patch_version.py
+++ scripts/patch_version.py
@@ -1,12 +1,13 @@
 import sys
 
 import tomlkit
+from time import time
 
 
 def run() -> None:
-    if len(sys.argv) != 3:
-        raise Exception("usage: patch_version.py <patch> <branch>")
-    _, patch, branch = sys.argv
+    if len(sys.argv) != 2:
+        raise Exception("usage: patch_version.py <branch>")
+    _, branch = sys.argv
     if branch == "master":
         print("not patching master branch")
         return
@@ -15,7 +16,7 @@ def run() -> None:
         content = tomlkit.load(py_project)
         content["tool"]["poetry"][  # type: ignore
             "version"
-        ] = f'{content["tool"]["poetry"]["version"]}-{patch}'  # type: ignore
+        ] = f'{content["tool"]["poetry"]["version"]}-{int(time())}'  # type: ignore
     if content is None:
         raise Exception('Could not find "tool.poetry.version" in pyproject.toml')
     with open("pyproject.toml", "w") as py_project:
